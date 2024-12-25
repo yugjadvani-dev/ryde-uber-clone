632c76b84136066f556bd29ff5d175c17aee4a8d
Yug Jadvani <yugjadvani.dev@gmail.com>
2024-12-26 01:39:40 +0530

    Remove body-parser and replace with express.urlencoded

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
